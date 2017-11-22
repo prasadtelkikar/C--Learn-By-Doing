87db8fb1359368b88617be0c9603a7c9cd4ea1c3
prasadtelkikar <telkikar.prasad@gmail.com>
2017-11-22 17:01:15 +0530

    Half done: Reodering; Need to solve error

--- C-Sharp-Data-Structure/C-Sharp-Data-Structure/Linked-List-Problems/ReorderLinkedList.cs
+++ C-Sharp-Data-Structure/C-Sharp-Data-Structure/Linked-List-Problems/ReorderLinkedList.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/* Problem - 54 : Reorder A1, An, A2, An-2,... */
 namespace C_Sharp_Data_Structure.Linked_List_Problems
 {
     public class ReorderLinkedList
@@ -54,11 +54,60 @@ namespace C_Sharp_Data_Structure.Linked_List_Problems
         private Node ReorderList()
         {
             Node currentNode = head;
+            Node fastNode = head;
+            Node fastEnd = null;
+
+            Node slowNode = head;
+            Node result = null;
 
             if(currentNode == null)
             {
                 return currentNode;
             }
+
+            while(currentNode!= null && currentNode.next != null)
+            {
+                currentNode = currentNode.next.next;
+                slowNode = slowNode.next;
+            }
+
+            Node reverseList = ReverseList(slowNode.next);
+            currentNode = new Node(0);
+
+            //TODO: Error: Last element is going into loop, so loop is going into infinity
+            while(fastNode != null || reverseList != null)
+            {
+                if(fastNode != null)
+                {
+                    currentNode.next = fastNode;
+                    currentNode = currentNode.next;
+                    fastNode = fastNode.next;
+                }
+                if(reverseList != null)
+                {
+                    currentNode.next = reverseList;
+                    currentNode = currentNode.next;
+                    reverseList = reverseList.next;
+                }
+            }
+            currentNode = currentNode.next;
+            return currentNode;
+        }
+
+        private Node ReverseList(Node slowNode)
+        {
+            Node currentNode = slowNode;
+            Node previousNode = null;
+            Node nextNode = null;
+
+            while(currentNode != null)
+            {
+                nextNode = currentNode.next;
+                currentNode.next = previousNode;
+                previousNode = currentNode;
+                currentNode = nextNode;
+            }
+            return previousNode;
         }
 
         public static void Main(string[] args)
@@ -71,6 +120,7 @@ namespace C_Sharp_Data_Structure.Linked_List_Problems
             reorderList.InsertAtEnd(5);
 
             reorderList.Display();
+            Node result = reorderList.ReorderList();
             Console.ReadKey();
         }
 
