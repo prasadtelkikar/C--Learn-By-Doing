ca794d6ed4227ec5762205eb861dc5c43fd87915
Prasad L Telkikar <telkikar.prasad@gmail.com>
2018-03-03 23:58:50 +0530

    Stack using dynamuc array

--- C-Sharp-Data-Structure/C-Sharp-Data-Structure/stack/StackUsingDynamicArray.cs
+++ C-Sharp-Data-Structure/C-Sharp-Data-Structure/stack/StackUsingDynamicArray.cs
@@ -9,7 +9,7 @@ namespace C_Sharp_Data_Structure.Stack
     public class StackUsingDynamicArray
     {
         int top = -1;
-        readonly int capacity = 10;
+        int capacity = 10;
         int[] array;
 
         public StackUsingDynamicArray(int capacity)
@@ -25,16 +25,50 @@ namespace C_Sharp_Data_Structure.Stack
 
         public bool IsFullStack()
         {
-            return (top == capacity);
+            return (top + 1 == capacity);
         }
 
         public int Pop()
         {
-            return 0;
+            if (IsEmptyStack())
+                throw new Exception("Stack underflow");
+            int topElement =  array[top--];
+            if(array.Length - 1 == capacity / 4)
+            {
+                var t = array;
+                array = new int[capacity / 2];
+                Array.Copy(t, array, capacity / 2);
+            }
+            return topElement;
+        }
+
+        public void Push(int data)
+        {
+            if(IsFullStack())
+            {
+                capacity *= 2;
+                var temp = array;
+                array = new int[capacity];
+                Array.Copy(temp, array, temp.Length);
+            }
+            array[++top] = data;
         }
 
         public static void Main(string[] args)
         {
+            StackUsingDynamicArray stackDynamic = new StackUsingDynamicArray(4);
+            stackDynamic.Push(1);
+            stackDynamic.Push(2);
+            stackDynamic.Push(3);
+            stackDynamic.Push(4);
+            stackDynamic.Push(5);
+
+            Console.WriteLine(stackDynamic.Pop());
+            Console.WriteLine(stackDynamic.Pop());
+            Console.WriteLine(stackDynamic.Pop());
+            Console.WriteLine(stackDynamic.Pop());
+            Console.WriteLine(stackDynamic.Pop());
+            Console.WriteLine(stackDynamic.Pop());  /* Underflow */
 
         }
     }
