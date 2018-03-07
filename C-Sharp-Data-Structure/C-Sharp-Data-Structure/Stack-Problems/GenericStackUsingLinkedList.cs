8697001c827665602ebdd2678a650d358b17557a
Prasad L Telkikar <telkikar.prasad@gmail.com>
2018-03-07 22:07:02 +0530

    Started implementing generic implemenation of stack

---
+++ C-Sharp-Data-Structure/C-Sharp-Data-Structure/Stack-Problems/GenericStackUsingLinkedList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Data_Structure.Stack_Problems
+{
+    public class GenericStackUsingLinkedList
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Generic stack implementation");
+        }
+    }
+}
