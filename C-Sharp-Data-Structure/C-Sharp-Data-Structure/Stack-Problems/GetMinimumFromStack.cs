47a020cd7d3ca63bc60dd58bc19dff121e45e9f7
Prasad L Telkikar <telkikar.prasad@gmail.com>
2018-03-11 17:32:50 +0530

    Solved Get minimum from stack O(1)

--- C-Sharp-Data-Structure/C-Sharp-Data-Structure/Stack-Problems/GetMinimumFromStack.cs
+++ C-Sharp-Data-Structure/C-Sharp-Data-Structure/Stack-Problems/GetMinimumFromStack.cs
@@ -8,43 +8,97 @@ namespace C_Sharp_Data_Structure.Stack_Problems
 {
     public class GetMinimumFromStack
     {
-        GenericStack<int> mainStack;
-        GenericStack<int> minStack;
+        int topMain;
+        int topMin;
+        private StackNode mainHead;
+        private StackNode minHead;
 
         public GetMinimumFromStack()
         {
-            mainStack = new GenericStack<int>();
-            minStack = new GenericStack<int>();
+            topMain = -1;
+            topMin = -1;
+        }
+
+        private StackNode CreateStack(int data)
+        {
+            return new StackNode(data);
+        }
+        public bool IsEmptyStack()
+        {
+            return (topMain == -1 || topMin == -1);
+        }
+
+        public void Push(int data)
+        {
+            StackNode oldNode = mainHead;
+            StackNode newNode = CreateStack(data);
+            newNode.nextNode = oldNode;
+            mainHead = newNode;
+            StackNode oldMinNode = minHead;
+            StackNode newMinNode;
+            if (IsEmptyStack() || MinPeek()>= data)
+                newMinNode = CreateStack(data);
+            else
+            {
+                newMinNode = CreateStack(MinPeek());
+                newMinNode.nextNode = oldMinNode;
+            }
+
+            newMinNode.nextNode = oldMinNode;
+            minHead = newMinNode;
+            topMain++;
+            topMin++;
+        }
+
+        public int Pop()
+        {
+            if (IsEmptyStack())
+                return -1;
+            int temp = mainHead.data;
+            mainHead = mainHead.nextNode;
+            minHead = minHead.nextNode;
+            topMin--;
+            topMain--;
+            return temp;
+        }
+
+        public int MinPeek()
+        {
+            if (IsEmptyStack())
+                return int.MaxValue;
+
+            return minHead.data;
+        }
+
+        public int GetMinimum()
+        {
+            return minHead.data;
         }
 
         public static void Main(string[] args)
         {
-            string array = Console.ReadLine();
-            GetMinimumFromStack minFromStack = new GetMinimumFromStack();
-//            GenericStack<int> mainStack = CreateMainStack(array);
-            int min = minFromStack.GetMinimum(array);
-            Console.WriteLine(min);
+            GetMinimumFromStack getMin = new GetMinimumFromStack();
+            getMin.Push(2);
+            getMin.Push(6);
+            getMin.Push(4);
+            getMin.Push(1);
+            getMin.Push(5);
+
+            Console.WriteLine(getMin.GetMinimum());
+            getMin.Pop();
+            getMin.Pop();
+            Console.WriteLine(getMin.GetMinimum());
             Console.ReadKey();
         }
-
-        private int GetMinimum(string array)
+        private class StackNode
         {
-            int[] elements = Array.ConvertAll(array.Split(' '), Int32.Parse);
-            foreach (int element in elements)
+            public int data;
+            public StackNode nextNode;
+            public StackNode(int data)
             {
-                if (mainStack.IsStackEmpty() || (!mainStack.IsStackEmpty() && minStack.Peek() > element))
-                {
-                    mainStack.Push(element);
-                    minStack.Push(element);
-                }
-                else 
-                {
-                    int top = minStack.Peek();
-                    minStack.Push(top);
-                    mainStack.Push(element);
-                }
+                this.data = data;
+                nextNode = null;
             }
-            return minStack.Peek();
         }
     }
 }
