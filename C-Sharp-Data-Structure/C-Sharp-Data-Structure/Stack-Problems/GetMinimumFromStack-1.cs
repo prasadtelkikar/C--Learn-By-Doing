c7db332555e358ffd2324d076b2fae01baf503f2
Prasad L Telkikar <telkikar.prasad@gmail.com>
2018-03-12 23:31:21 +0530

    Solved GetMinimum with less space complexity and palindrom program

---
+++ C-Sharp-Data-Structure/C-Sharp-Data-Structure/Stack-Problems/GetMinimumFromStack-1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Data_Structure.Stack_Problems
+{
+    public class GetMinimumFromStack_1
+    {
+        int mainTop;
+        int minTop;
+        StackNode mainHead;
+        StackNode minHead;
+
+        public GetMinimumFromStack_1()
+        {
+            mainTop = -1;
+            minTop = -1;
+        }
+        private StackNode CreateNode(int data)
+        {
+            return new StackNode(data);
+        }
+        public bool IsEmptyMainStack()
+        {
+            return (mainTop == -1);
+        }
+        public bool IsEmptyMinStack()
+        {
+            return (minTop == -1);
+        }
+
+        public void Push(int data)
+        {
+            StackNode oldMainHead = mainHead;
+            StackNode newNode = CreateNode(data);
+            newNode.nextNode = mainHead;
+            mainHead = newNode;
+            StackNode oldMinHead;
+            StackNode newMinNode;
+
+            if (IsEmptyMinStack() || minHead.data >= data)
+            {
+                oldMinHead = minHead;
+                newMinNode = CreateNode(data);
+                newMinNode.nextNode = oldMinHead;
+                minHead = newMinNode;
+                minTop++;
+            }
+            mainTop++;
+        }
+
+        public int Pop()
+        {
+            int temp;
+            if(IsEmptyMainStack())
+            {
+                Console.WriteLine("Underflow");
+                return -1;
+            }
+            else
+            {
+                temp = mainHead.data;
+                if(temp == minHead.data)
+                {
+                    minHead = minHead.nextNode;
+                    minTop--;
+                }
+                mainHead = mainHead.nextNode;
+                mainTop--;
+            }
+            return temp;
+        }
+
+        public int GetMinimum()
+        {
+            if (IsEmptyMinStack())
+            {
+                Console.WriteLine("Min stack is empty");
+                return int.MaxValue; //Returning garbage value if stack is null
+            }
+            return minHead.data;
+        }
+        public static void Main(string[] args)
+        {
+            GetMinimumFromStack_1 getMin = new GetMinimumFromStack_1();
+            getMin.Push(2);
+            getMin.Push(6);
+            getMin.Push(4);
+            getMin.Push(1);
+            getMin.Push(5);
+
+            Console.WriteLine(getMin.GetMinimum());
+            getMin.Pop();
+            getMin.Pop();
+            Console.WriteLine(getMin.GetMinimum());
+            Console.ReadKey();
+        }
+        private class StackNode
+        {
+            public int data;
+            public StackNode nextNode;
+
+            public StackNode(int data)
+            {
+                this.data = data;
+                nextNode = null;
+            }
+        }
+    }
+}
---
+++ C-Sharp-Data-Structure/C-Sharp-Data-Structure/Stack-Problems/Palindrom.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Data_Structure.Stack_Problems
+{
+    public class Palindrom
+    {
+        public static bool IsPalindrom(string input)
+        {
+            for (int i = 0; i < input.Length/2; i++)
+            {
+                if (input[i] != input[input.Length - i - 1])
+                    return false;
+            }
+            return true;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string to check palindrom? :");
+            string input = Console.ReadLine();
+            bool isPalindrom = IsPalindrom(input);
+            Console.WriteLine(isPalindrom ? "String is palindrom" : "String is not a palindrom");
+            Console.ReadKey();
+        }
+    }
+}
