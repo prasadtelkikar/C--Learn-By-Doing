31cd6dee373beaf383b3755ab81ae1f5b2e541ed
Prasad L Telkikar <telkikar.prasad@gmail.com>
2018-04-01 22:11:09 +0530

    Solved M stacks in single array

---
+++ C-Sharp-Data-Structure/C-Sharp-Data-Structure/Stack-Problems/MStacksInOneArray.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Data_Structure.Stack_Problems
+{
+    public class MStacksInOneArray
+    {
+        int m, size;
+        int[] data;
+        int[] topIndex;
+        int[] baseIndex;
+        int[] currentIndex;
+        public MStacksInOneArray(int arrSize, int stackNo)
+        {
+            m = stackNo;
+            size = arrSize;
+            data = new int[size];
+            topIndex = new int[m];
+            baseIndex = new int[m];
+            currentIndex = new int[m];
+            for (int i = 0; i < m; i++)
+            {
+                if (i != 0)
+                {
+                    baseIndex[i] = topIndex[i - 1];
+                    currentIndex[i] = baseIndex[i];
+                }
+                topIndex[i] = ((i + 1) * size) / m;
+            }
+        }
+        
+        public MStacksInOneArray() : this(10, 5)
+        {
+
+        }
+        public void Push(int stackIndex, int value)
+        {
+            if(stackIndex <= m)
+            {
+                int currentindex = currentIndex[stackIndex-1];
+                int topindex = topIndex[stackIndex-1];
+
+                if (IsFullStack(stackIndex - 1))
+                {
+                    Console.WriteLine("Stack is full");
+                    return;
+                }
+                data[currentIndex[stackIndex - 1]++] = value;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+
+        private bool IsFullStack(int index)
+        {
+            return currentIndex[index] == topIndex[index];
+        }
+
+        public int Pop(int stackIndex)
+        {
+            if(stackIndex <= m)
+            {
+                if (stackIndex == 1 && currentIndex[stackIndex - 1] == 0 && topIndex[stackIndex - 1] == 0)
+                {
+                    Console.WriteLine("Stack is empty");
+                    return int.MinValue;
+                }
+                else
+                {
+                    int prevBaseIndex = baseIndex[stackIndex - 2];
+                    int currentindex = currentIndex[stackIndex - 1];
+                    if(prevBaseIndex == currentindex)
+                    {
+                        Console.WriteLine("Stack is empty");
+                        return int.MinValue;
+                    }
+                }
+                int value = data[currentIndex[stackIndex - 1]--];
+                return value;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input while pop function");
+                return int.MinValue;
+            }
+        }
+
+        public int Peek(int stackIndex)
+        {
+            if(stackIndex <= m)
+            {
+                return data[currentIndex[stackIndex - 1]];
+            }
+            return int.MinValue;
+        }
+
+        public static void Main(string[] args)
+        {
+            MStacksInOneArray mstack = new MStacksInOneArray(20,2);
+            mstack.Push(1, 5);
+            mstack.Push(1, 10);
+            mstack.Push(1, 15);
+            mstack.Push(2, 20);
+            mstack.Push(2, 25);
+            mstack.Push(2, 30);
+            
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C-Sharp-Data-Structure/C-Sharp-Data-Structure/Stack-Problems/ThreeStackOneArray.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Data_Structure.Stack_Problems
+{
+    //Copied from https://github.com/lakshmikantdeshpande/Data_Structures_in_Java/blob/master/Java%20Data%20Structures/src/stack_problems/ThreeStacksInArray.java
+    public class ThreeStackOneArray
+    {
+        int[] data;
+        int topOne, topTwo, topThree, baseThree, size;
+
+        public ThreeStackOneArray(int size)
+        {
+            this.size = size;
+            data = new int[size];
+            topOne = 0;
+            topTwo = size - 1;
+            baseThree = (size - 1) / 2;
+            topThree = baseThree;
+        }
+        public ThreeStackOneArray():this(10)
+        {
+        }
+        
+        public void Push(int stackIndex, int data1)
+        {
+            if(stackIndex == 1)
+            {
+                if(topOne+1 == baseThree)
+                {
+                    if (IsMiddleStackRightShiftable())
+                    {
+                        ShiftMiddleStackToRight();
+                        data[topOne] = data1;
+                        topOne++;
+                    }
+                    else
+                    {
+                        throw new Exception("First stack is full");
+                    }
+                }
+                else
+                {
+                    data[topOne] = data1;
+                    topOne++;
+                }
+            }
+            else if(stackIndex == 2)
+            {
+                if (topTwo - 1 == baseThree)
+                {
+                    if (IsMiddleStackLeftShiftable())
+                    {
+                        ShiftMiddleStackToLeft();
+                        data[topTwo--] = data1;
+                    }
+                    else
+                    {
+                        throw new Exception("second stack is full");
+                    }
+                }
+                else
+                    data[topTwo--] = data1;
+
+            }
+            else if(stackIndex == 3)
+            {
+                if(topTwo - 1 == topThree)
+                {
+                    if (IsMiddleStackRightShiftable())
+                    {
+                        ShiftMiddleStackToRight();
+                        data[topThree] = data1;
+                        topThree++;
+                    }
+                    else
+                    {
+                        throw new Exception("Stack 3 is full");
+                    }
+                }
+                else
+                {
+                    data[topThree] = data1;
+                    topThree++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+
+        public int Pop(int stackIndex)
+        {
+            if(stackIndex == 1)
+            {
+                if(topOne == -1)
+                {
+                    throw new Exception("First stack is empty");
+                }
+                int value = data[topOne];
+                data[topOne--] = int.MinValue;
+                return value;
+            }
+            else if(stackIndex == 2)
+            {
+                if(topTwo == this.size)
+                {
+                    throw new Exception("Second stack is empty");
+                }
+                int value = data[topTwo];
+                data[topTwo++] = int.MinValue;
+                return value;
+            }
+            else if(stackIndex == 3)
+            {
+                if (topThree == baseThree)
+                    throw new Exception("Third stack is empty");
+                int value = data[topThree];
+                data[topThree--] = int.MinValue;
+                return value;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+                return int.MinValue;
+            }
+        }
+        public int Peek(int stackIndex)
+        {
+            if (stackIndex == 1)
+            {
+                if (topOne == -1)
+                    throw new Exception("first stack is empty");
+                return data[topOne];
+            }
+            else if (stackIndex == 2)
+            {
+                if (topTwo == this.size)
+                    throw new Exception("second stack is empty");
+                return data[topTwo];
+            }
+            else if (stackIndex == 3)
+            {
+                if (topThree == baseThree)
+                    throw new Exception("Third stack is empty");
+                return data[topThree];
+            }
+            else
+                return int.MinValue;
+        }
+        public bool IsEmpty(int stackIndex)
+        {
+            if (stackIndex == 1)
+                return topOne == -1;
+            else if (stackIndex == 2)
+                return topTwo == this.size;
+            else if (stackIndex == 3)
+                return topThree == baseThree;
+            else
+                return false;
+        }
+        private void ShiftMiddleStackToRight()
+        {
+            for(int i = topThree; i >= baseThree; i--)
+                data[i + 1] = data[i];
+            data[baseThree++] = int.MinValue;
+            topThree++;
+        }
+
+        private void ShiftMiddleStackToLeft()
+        {
+            for (int i = baseThree + 1; i <= topThree; i++)
+                data[i] = data[i + 1];
+            data[topThree--] = int.MinValue;
+            baseThree--;
+        }
+
+        private bool IsMiddleStackRightShiftable()
+        {
+            return topOne + 1 < baseThree;
+        }
+
+        private bool IsMiddleStackLeftShiftable()
+        {
+            return topThree + 1 < topTwo;
+        }
+
+        public static void Main(String[] args)
+        {
+            ThreeStackOneArray three = new ThreeStackOneArray();
+            three.Push(1, 1);
+            three.Push(1, 1);
+            three.Push(1, 1);
+            Console.ReadKey();
+        }
+    }
+}
