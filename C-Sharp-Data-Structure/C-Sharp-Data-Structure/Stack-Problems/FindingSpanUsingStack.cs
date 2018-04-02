224ea6b483d1ead08e2d7d131289eadc0f11bc01
Prasad L Telkikar <telkikar.prasad@gmail.com>
2018-04-02 22:35:16 +0530

    Solved finding span without using stack

---
+++ C-Sharp-Data-Structure/C-Sharp-Data-Structure/Stack-Problems/FindingSpanUsingStack.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Data_Structure.Stack_Problems
+{
+    public class FindingSpanUsingStack
+    {
+        public FindingSpanUsingStack() { }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C-Sharp-Data-Structure/C-Sharp-Data-Structure/Stack-Problems/FindingSpans.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Data_Structure.Stack_Problems
+{
+    public class FindingSpans
+    {
+        int[] input;
+        int[] span;
+
+        public FindingSpans()
+        {
+            Console.WriteLine("Enter input array");
+            input = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            span = new int[input.Length];
+        }
+
+        public int[] FindMaxSpan()
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                int j = 1;
+                while (j <= i && input[j] < input[i])
+                    j += 1;
+                span[i] = j;
+            }
+            return span ;
+        }
+
+        public static void Main(string[] args)
+        {   
+            FindingSpans fs = new FindingSpans();
+            int[] result = fs.FindMaxSpan();
+
+            //print result in tabular format. Input \t span
+            Console.WriteLine("Input    Span");
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(fs.input[i] + "\t" + result[i]);
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
