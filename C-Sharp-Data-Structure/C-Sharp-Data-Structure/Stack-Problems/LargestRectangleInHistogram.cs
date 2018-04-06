9611eb34a8465a081b77912a67146e3989430588
Prasad L Telkikar <telkikar.prasad@gmail.com>
2018-04-06 22:52:17 +0530

    Started with largest rectangle brute force;Need improvement buggy code

--- C-Sharp-Data-Structure/C-Sharp-Data-Structure/Stack-Problems/LargestRectangleInHistogram.cs
+++ C-Sharp-Data-Structure/C-Sharp-Data-Structure/Stack-Problems/LargestRectangleInHistogram.cs
@@ -8,5 +8,36 @@ namespace C_Sharp_Data_Structure.Stack_Problems
 {
     public class LargestRectangleInHistogram
     {
+        int[] input;
+        public LargestRectangleInHistogram(int[] input)
+        {
+            this.input = input;
+        }
+        public int FindLargestHistogram()
+        {
+            int maxArea = int.MinValue;
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                int j = i + 1;
+                int count = 1;
+                while(j < input.Length && input[j] >= input[i])
+                {
+                    count++;
+                    j++;
+                }
+                int area = count * input[i];
+                maxArea = (maxArea < area) ? area : maxArea;
+            }
+            return maxArea;
+        }
+        public static void Main(string[] args)
+        {
+            string[] temp = Console.ReadLine().Split(' ');
+            int[] input = Array.ConvertAll(temp, int.Parse);
+            LargestRectangleInHistogram largest = new LargestRectangleInHistogram(input);
+            Console.WriteLine("Maximum area:" + largest.FindLargestHistogram());
+            //Console.WriteLine("Hello world");
+            Console.ReadLine();
+        }
     }
 }
