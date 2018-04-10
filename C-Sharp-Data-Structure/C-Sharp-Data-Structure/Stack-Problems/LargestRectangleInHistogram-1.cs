69ffca99193efc7ac7f25dfa9ed3250770f1c19b
Prasad L Telkikar <telkikar.prasad@gmail.com>
2018-04-10 23:21:09 +0530

    Added largest rectangle with stack and stack grows or not

--- C-Sharp-Data-Structure/C-Sharp-Data-Structure/Stack-Problems/LargestRectangleInHistogram-1.cs
+++ C-Sharp-Data-Structure/C-Sharp-Data-Structure/Stack-Problems/LargestRectangleInHistogram-1.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace C_Sharp_Data_Structure.Stack_Problems
 {
+    //http://k2code.blogspot.in/2013/11/maximum-area-rectangle-in-histogram.html
     public class LargestRectangleInHistogram_1
     {
         int top;
@@ -44,16 +45,62 @@ namespace C_Sharp_Data_Structure.Stack_Problems
             if (IsEmptyStack())
             {
                 Console.WriteLine("Stack underflow");
-                return int.MinValue;
+                return 0;
             }
             int value = head.data;
             head = head.nextData;
+            top--;
             return value;
+        }
+
+        public int Peek()
+        {
+            if (IsEmptyStack())
+            {
+                Console.WriteLine("Stack underflow");
+                return int.MinValue;
+            }
+            return head.data;
+        }
+
+        private int FindMaxArea(int[] input)
+        {
+            int maxArea = 0;
+            foreach (int inp in input)
+            {
+                if (IsEmptyStack() || inp > Peek())
+                    Push(inp);
+                else
+                {
+                    int count = 0;
+                    while(!IsEmptyStack() && Peek() > inp)
+                    {
+                        count++;
+                        int value = Pop();
+                        maxArea = Math.Max(maxArea, (value * count));
+                    }
+                    for (int i = 0; i <= count; i++)
+                    {
+                        Push(inp);
+                    }
+                }
+            }
+
+            int countLast = 0;
+            while (!IsEmptyStack())
+            {
+                countLast++;
+                maxArea = Math.Max(maxArea, Pop() * countLast);
+            }
+            return maxArea;
         }
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            int[] input = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+            LargestRectangleInHistogram_1 largestRect = new LargestRectangleInHistogram_1();
+            int maxArea = largestRect.FindMaxArea(input);
+            Console.WriteLine("Area of largest rectangle in histogram = " + maxArea);
             Console.ReadKey();
         }
 
---
+++ C-Sharp-Data-Structure/C-Sharp-Data-Structure/Stack-Problems/WhetherStackGrows.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Data_Structure.Stack_Problems
+{
+    public class WhetherStackGrows
+    {
+        public static unsafe void Main(string[] args)
+        {
+            int temp = 10;
+            bool result = CheckStackGrows(&temp);
+            Console.WriteLine(result ? "Stack is growing upward" : "Stack is growing downward");
+            Console.ReadKey();
+        }
+
+        private static unsafe bool CheckStackGrows(int* ptr)
+        {
+            IntPtr intPtr = (IntPtr)ptr;
+            long intPtrCast = (long)intPtr;
+            int nextTemp = 20;
+            //Created pointer to store address of variable
+            int* nTprt = &nextTemp;
+            //Converted to IntPtr
+            IntPtr nextTempPtr = (IntPtr)nTprt;
+            //Cast to long for comparing two pointer address
+            long nextTempCast = (long)nextTempPtr;
+
+            Console.WriteLine("Address of first pointer = " + intPtrCast);
+            Console.WriteLine("Address of second pointer = " + nextTempCast);
+            if (nextTempCast > intPtrCast)
+                return true;
+            return false;
+            //throw new NotImplementedException();
+        }
+    }
+}
