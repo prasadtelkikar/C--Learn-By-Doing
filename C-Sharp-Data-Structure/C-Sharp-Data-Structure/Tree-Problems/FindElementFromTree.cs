21d90fd3bc32038cc61cc06e7881bdeb13112803
Prasad L Telkikar <telkikar.prasad@gmail.com>
2018-05-09 23:01:14 +0530

    Started working on non recursive form of Finding element from tree

--- C-Sharp-Data-Structure/C-Sharp-Data-Structure/Tree-Problems/FindElementFromTree.cs
+++ C-Sharp-Data-Structure/C-Sharp-Data-Structure/Tree-Problems/FindElementFromTree.cs
@@ -8,10 +8,40 @@ namespace C_Sharp_Data_Structure.Tree_Problems
 {
     public class FindElementFromTree
     {
+        private BSTNode rootNode;
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the tree");
+            FindElementFromTree findElement = new FindElementFromTree();
+            findElement.rootNode = new BSTNode(1);
+            findElement.rootNode.leftNode = new BSTNode(2);
+            findElement.rootNode.rightNode = new BSTNode(3);
+            findElement.rootNode.leftNode.leftNode = new BSTNode(4);
+            findElement.rootNode.leftNode.rightNode = new BSTNode(5);
+            findElement.rootNode.rightNode.leftNode = new BSTNode(6);
+            findElement.rootNode.rightNode.rightNode = new BSTNode(7);
+
+            bool result = findElement.FindGivenElementFromTree(findElement.rootNode, 3);
+            Console.WriteLine(result ? "Element present in tree" : "Element not available in tree");
+
             Console.ReadKey();
+        }
+
+        //Buggy code
+        private bool FindGivenElementFromTree(BSTNode rootNode, int data)
+        {
+            if (rootNode == null)
+                return false;
+            else
+            {
+                if (rootNode.data == data)
+                    return true;
+
+                if (FindGivenElementFromTree(rootNode.leftNode, data))
+                    return true;
+                else
+                    FindGivenElementFromTree(rootNode.rightNode, data);
+            }
+            return false;
         }
 
         #region BST Class
